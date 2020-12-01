8e01c0886c56c01e3d1445cda49847cdd7491a4a
iapilgrim <pilgrim2go@gmail.com>
2020-12-01 19:54:06 +0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Van"}
+		res := &response{Message: "Hello NC"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
